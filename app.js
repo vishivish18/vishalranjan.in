a9226958dc5f69a80c319a72c7110fda67ae38d1
Vishal <Specta@Vishals-MacBook-Pro.local>
2016-06-05 23:33:22 +0530

    setting up with jade

--- app.js
+++ app.js
@@ -3,11 +3,16 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
-
-app.use('/', require('./app/controllers/static'))
-
+var path = require('path')
 
 
+app.set('views', __dirname + '/public/views')
+app.set('view engine', 'jade')
+app.use(express.static(path.resolve('public/assets/')))
+app.get('/', function(req, res) {
+    //res.sendFile(path.resolve('public/index.html'));
+    res.render('layout', { title: 'Home' })
+});
 var port = process.env.PORT || 1805
 var server = app.listen(port, function() {
     console.log('Magic happens at  ', port);
